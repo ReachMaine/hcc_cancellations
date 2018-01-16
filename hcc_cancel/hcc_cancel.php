e0118b890110226116db685fd3f36e53331e7323
Linda "zig" Speight <linda@reachmaine.com>
2018-01-15 19:39:16 -0500

    take into account timezone

--- hcc_cancel/hcc_cancel.php
+++ hcc_cancel/hcc_cancel.php
@@ -30,15 +30,16 @@ if (!function_exists('hcc_cancel_link')) {
   			'formid' => '6',
         'field' => '2',
         'link' => 'www.ellsworthamericaninc.com/view-cancellations/',
+        'image' => plugin_dir_url(__FILE__)."/cancellation-button.jpg",
   		), $atts, 'hcc_cancel' );
   	global $wpdb;
-
     $out_str = "";
     $link_text = $atts['link_text'];
     $formid = $atts['formid'];
     $blogid = $atts['blogid'];
     $fieldid = $atts['field'];
     $link =  $atts['link'];
+    $img = $atts['image'];
     //$table_name = 'ea_14_rg_lead_detail'; // for ea multisite $wpdb->base_prefix
     if ($blogid) {
         $table_name = $wpdb->base_prefix.$blogid."_rg_lead_detail";
@@ -48,18 +49,28 @@ if (!function_exists('hcc_cancel_link')) {
 
 
     $calquery = "SELECT * from $table_name where form_id='$formid' and field_number='$fieldid' ";
-    $today_dt = new DateTime('today');
+    $today_dt = new DateTime('America/New_York');
     $today_str = $today_dt->format("Y-m-d");
-    $tomorrow_dt =  new DateTime('tomorrow');
+    $tomorrow_dt = new DateTime('America/New_York');
+    $tomorrow_dt -> add(new DateInterval('P1D')); // add one day
+
     $tomorrow_str = $tomorrow_dt->format("Y-m-d");
     $calquery .= " and value in ('$today_str', '$tomorrow_str' )";
+    //echo $calquery;
     $calresult = $wpdb->get_results($calquery);
     //echo "<pre>"; var_dump($calresult); echo "</pre>";
     $out_str = "";
+
     if ($calresult) {
-      $out_str = "<a href='//$link' class='hcc_cancel_link'>$link_text</a>";
+      $out_str = "<a href='//$link' class='hcc_cancel_link'>";
+      if ($img) {
+        $out_str .= '<img clas="hcc_cancel_image" src="'.$img.'">';
+      } else {
+        $out_str .= $link_text;
+      }
+      $out_str .= "</a>";
     } else {
-      $out_str = "<p>nope.</p>";
+      $out_str .= "<p>nope.</p>";
     }
      //return $calquery.$out_str;
   	return $out_str;
